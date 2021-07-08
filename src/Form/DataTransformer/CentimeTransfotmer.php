71cdbc3eb0b24119fe7531638e0fae09e9d9781c
micka <mickavende@gmail.com>
2021-07-08 16:40:40 +0200

    composant form

---
+++ src/Form/DataTransformer/CentimeTransfotmer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+
+class CentimeTransformer implements DataTransformerInterface
+{
+public function transform($value)
+{
+    if ( null===$value)
+    {
+        return;
+    }
+
+    return $value/100;
+}
+
+
+public function reverseTransform($value)
+{
+    if (null=== $value)
+    {
+        return;
+    }
+
+    return $value*100;
+}
+}
--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,13 +4,17 @@ namespace App\Form;
 
 use App\Entity\Category;
 use App\Entity\Products;
+use App\Form\DataTransformer\CentimeTransformer;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProductType extends AbstractType
@@ -32,22 +36,70 @@ class ProductType extends AbstractType
                    "label"=>"prix du produit",
                    "attr"=>[   
                        "placeholder"=>"tapez le prix en €"
-                   ]
+                   ],
+                   'divisor'=>100
                    ])
 
                    ->add('mainPicture',UrlType::class,[
                        "label"=>"Image du produit",
                        "attr"=>["placHolder"=>"Taper une Url dimage !"]
                    ])
-
                    ->add("category",EntityType::class,[
-                   "label"=>"catégorie",
-                  "placeholder"=>"--choisir une categorie--",
-                   "class" => Category::class,
-                   "choice_label"=>"name"
-                   ]);
-    }
+                    "label"=>"catégorie",
+                   "placeholder"=>"--choisir une categorie--",
+                    "class" => Category::class,
+                    "choice_label"=>"name"
+                    ]);
+                    // $builder->get('prix')->addModelTransformer(new CentimeTransformer);
 
+                //     $builder->addEventListener(FormEvents::POST_SUBMIT, function(FormEvent $event){
+                   
+                //        $product=$event->getData();
+
+                //     if ($product->getPrix() !==null){
+                //         $product->setPrix ($product->getPrix()*100);
+                //     }
+                 
+
+                //     });
+
+                   
+
+                //    $builder->addEventListener(FormEvents::PRE_SET_DATA,function(FormEvent $event){
+                //        $form=$event->getForm();
+
+                //        /** @var Products */
+                //        $product =$event->getData();
+                       
+                //        if($product->getPrix() !==null){  
+
+                //       $product->setPrix ($product->getPrix()/100);
+
+                    //    }
+                     
+
+
+
+
+
+
+
+
+                //        if($product->getId()===null){
+
+                //    $form->add("category",EntityType::class,[
+                //    "label"=>"catégorie",
+                //   "placeholder"=>"--choisir une categorie--",
+                //    "class" => Category::class,
+                //    "choice_label"=>"name"
+                //    ]);
+                    //    }
+
+                    
+
+                //    });
+
+                }
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([

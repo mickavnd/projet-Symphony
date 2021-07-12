09d8a2fda5544acc6a7c8beb9b10a9f393ca85e5
micka <mickavende@gmail.com>
2021-07-12 23:37:14 +0200

    authentification

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,18 +4,22 @@ namespace App\DataFixtures;
 
 use App\Entity\Category;
 use App\Entity\Products;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AppFixtures extends Fixture
 {
     protected $slugger;
+    protected $encoder;
     
-    public function __construct(SluggerInterface $slugger)
+    public function __construct(SluggerInterface $slugger, UserPasswordEncoderInterface $encoder)
     {
         $this->slugger=$slugger;
+        $this->encoder=$encoder;
     }
 
 
@@ -29,6 +33,35 @@ class AppFixtures extends Fixture
         $faker->addProvider(new \Liior\Faker\Prices($faker));
         $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
         $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
+
+        
+        $admin= new User;
+
+        $hash =$this->encoder->encodePassword($admin,"password");
+
+        $admin->setEmail("admin@gmail.com")
+               ->setPassword($hash)
+               ->setFullName("admin")
+               ->setRoles(['ROLE_ADMIN']);
+
+               $manager->persist($admin);
+
+        
+        for( $u=0; $u < 5; $u++ ){
+
+            $user= new User();
+            $hash =$this->encoder->encodePassword($user,"password");
+
+            $user->setEmail("user$u@gmail.com")
+                 ->setFullName($faker->name())
+                 ->setPassword($hash);
+
+                 $manager->persist($user);
+
+        }
+
+
+
 
         for($c =0; $c < 3;$c++){
             $category= new Category;
---
+++ src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+
+class LoginFormAuthenticator extends AbstractGuardAuthenticator
+{
+    protected $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder){
+       
+        $this->encoder=$encoder;
+    }
+    
+        
+    
+
+
+    public function supports(Request $request)
+    {
+        return $request->attributes->get('_route') === 'security_login' 
+        && $request->isMethod('POST');
+    }
+
+    public function getCredentials(Request $request)
+    {
+        return $request->get('login');//array avec 3 info
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        try{
+            return $userProvider->loadUserByUsername($credentials['email']);
+        }catch(UsernameNotFoundException $e){
+            throw new AuthenticationException("cette adresse email nes pas connu");
+        }
+       
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        // verifier que le mot de passe fourni , correspond bien au mot de passe  la base de  donnée 
+        $isValid= $this->encoder->isPasswordValid($user,$credentials['password']);
+
+        if(!$isValid){
+            throw new AuthenticationException(" les information de connexion ne corresponde pas ");
+        }
+        return true;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        $request->attributes->set(Security::AUTHENTICATION_ERROR,$exception);
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
+    {
+        return New RedirectResponse("/");
+    }
+
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        // todo
+    }
+
+    public function supportsRememberMe()
+    {
+        // todo
+    }
+}
